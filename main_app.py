0fe5c2fefe422ad5fcbc0490bf2b0faeb132eb31
Mesach Venegas <mesachman@gmail.com>
2021-12-07 13:33:32 -0800

    desarrollo del menu del app

--- main_app.py
+++ main_app.py
@@ -1,2 +1,13 @@
 from clases.cls_users import Users
-from clases.looger_info import log
+from clases.DAO import DAOAPP
+
+def main_menu():
+    home_menu = ['Crear Usuario', 'Ver usuarios', 'Actualizar usuario', 'Eliminar Usuario', 'Salir']
+    num = 1
+    for opt in home_menu:
+        print(f'{num}.- {opt}')
+
+
+
+if __name__ == '__main__':
+    main_menu()
